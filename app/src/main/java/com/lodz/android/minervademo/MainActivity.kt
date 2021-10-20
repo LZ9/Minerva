38cdc9ede095d74833a430baa0941f9414482dec
zhouL <lz.mms@foxmail.com>
2021-10-20 14:18:35 +0800

    添加首页UI

--- app/src/main/java/com/lodz/android/minervademo/MainActivity.kt
+++ app/src/main/java/com/lodz/android/minervademo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.lodz.android.minervademo
 
 import android.os.Bundle
 import android.view.View
+import com.lodz.android.corekt.anko.append
 import com.lodz.android.corekt.anko.getColorCompat
 import com.lodz.android.minervademo.databinding.ActivityMainBinding
 import com.lodz.android.pandora.base.activity.BaseActivity
@@ -17,12 +18,35 @@ class MainActivity : BaseActivity() {
     override fun findViews(savedInstanceState: Bundle?) {
         super.findViews(savedInstanceState)
         setTitleBar(getTitleBarLayout())
+        mBinding.savePathTv.text = getString(R.string.main_save_path).append(FileManager.getContentFolderPath())
+        mBinding.startBtn.isEnabled = true
+        mBinding.pauseBtn.isEnabled = false
     }
 
     private fun setTitleBar(titleBarLayout: TitleBarLayout) {
         titleBarLayout.needBackButton(false)
         titleBarLayout.setBackgroundColor(getColorCompat(R.color.color_00a1d5))
         titleBarLayout.setTitleName(R.string.app_name)
+    }
+
+    override fun setListeners() {
+        super.setListeners()
+
+        mBinding.configBtn.setOnClickListener {
+
+        }
+
+        mBinding.startBtn.setOnClickListener {
+
+        }
+
+        mBinding.stopBtn.setOnClickListener {
+
+        }
+
+        mBinding.pauseBtn.setOnClickListener {
+
+        }
     }
 
     override fun initData() {
